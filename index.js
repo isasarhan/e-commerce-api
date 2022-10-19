e7be685e265b8ddfb5be6ec0da64cb2514005661
Issa <isasarhan.lz@gmail.com>
2022-10-19 15:26:17 +0300

    adding missing routes

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 const dontenv = require('dotenv')
 const userRouter = require('./routes/user')
 const authRouter = require('./routes/auth')
 const productRouter = require('./routes/product')
+const orderRouter = require('./routes/order')
+const cartRouter = require('./routes/cart')
 
 dontenv.config()
 mongoose
     .connect(process.env.MONGO_URL)
-    .then(()=>console.log("Database connection successful"))
-    .catch((err)=>{
+    .then(() => console.log("Database connection successful"))
+    .catch((err) => {
         console.log(err);
     });
 
@@ -18,7 +20,9 @@ app.use(express.json());
 app.use("/api/auth", authRouter)
 app.use("/api/users", userRouter)
 app.use("/api/products", productRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/orders", orderRouter)
 
-app.listen(process.env.PORT || 5000, ()=>{
+app.listen(process.env.PORT || 5000, () => {
     console.log('backend server is running');
 })
